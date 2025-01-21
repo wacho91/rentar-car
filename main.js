ff8d97225dc557dcbf2c8cce79cd2cd957609380
wacho91 <criskol.71@gmail.com>
2025-01-21 11:38:55 -0500

    Script  for features

--- main.js
+++ main.js
@@ -63,3 +63,26 @@ ScrollReveal().reveal(".about__btn", {
     ...scrollRevealOption,
     delay: 2500,
 });
+
+const fleet1 = document.querySelector(".fleet__wrapper-1 .fleet__images");
+const fleet2 = document.querySelector(".fleet__wrapper-2 .fleet__images");
+
+const fleet1Content = Array.from(fleet1.children);
+const fleet2Content = Array.from(fleet2.children);
+
+fleet1Content.forEach((item) => {
+    const duplicateNode = item.cloneNode(true);
+    duplicateNode.setAttribute("aria-hidden", true);
+    fleet1.appendChild(duplicateNode);
+});
+
+fleet2Content.forEach((item) => {
+    const duplicateNode = item.cloneNode(true);
+    duplicateNode.setAttribute("aria-hidden", true);
+    fleet2.appendChild(duplicateNode);
+});
+
+ScrollReveal().reveal(".feature__card", {
+    ...scrollRevealOption,
+    interval: 500,
+});
