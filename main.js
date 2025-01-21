4b2d2277d7ef52c25a37cdf4011131c82062fb37
wacho91 <criskol.71@gmail.com>
2025-01-21 10:34:13 -0500

    Script for about

--- main.js
+++ main.js
@@ -16,3 +16,50 @@ navLinks.addEventListener("click", (e) => {
     navLinks.classList.remove("open");
     menuBtnIcon.setAttribute("class", "ri-menu-3-line");
 });
+
+const scrollRevealOption = {
+    distance: "50px",
+    origin: "bottom",
+    duration: 1000,
+};
+
+ScrollReveal().reveal(".header__image img", {
+    ...scrollRevealOption,
+    origin: "right",
+});
+
+ScrollReveal().reveal(".header__content h1", {
+    ...scrollRevealOption,
+    delay: 500,
+});
+
+ScrollReveal().reveal(".header__content .section__description", {
+    ...scrollRevealOption,
+    delay: 1000,
+});
+
+ScrollReveal().reveal(".about__image img", {
+    ...scrollRevealOption,
+    origin: "left",
+});
+
+ScrollReveal().reveal(".about__content .section__subheader", {
+    ...scrollRevealOption,
+    delay: 500,
+});
+
+ScrollReveal().reveal(".about__content .section__header", {
+    ...scrollRevealOption,
+    delay: 1000,
+});
+
+ScrollReveal().reveal(".about__content .section__description", {
+    ...scrollRevealOption,
+    delay: 1500,
+    interval: 500,
+});
+
+ScrollReveal().reveal(".about__btn", {
+    ...scrollRevealOption,
+    delay: 2500,
+});
